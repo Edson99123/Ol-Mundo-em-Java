9d0969b49e227a4f9fd1a75d45a805e59f07eaa8
edson <edson@h1n2ch3>
2021-04-27 19:59:13 -0500

    Criação do projeto

---
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,11 @@
+package cursojava.executavel;
+
+
+
+public class PrimeiraClasseJava {
+	/*Main é um metodo auto executavel em java*/
+	public static void main(String[] args) {
+		System.out.println("Meu primeiro programa em java - do Curso formação java web\nJava orientado a objetos!!");
+	}
+
+}
